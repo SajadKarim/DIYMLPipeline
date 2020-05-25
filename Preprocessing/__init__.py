a081e0f6a59c1459a6a6022da7038e58e64ff4b0
@sajad.karim <sajad.karim@gei.de>
2020-05-25 04:04:26 +0200

    Fix issues.

--- Preprocessing/__init__.py
+++ Preprocessing/__init__.py
@@ -7,7 +7,12 @@ import matplotlib.pyplot as plt
 import csv
 import re
 
-allTypes=['Numeric','Boolean','String','DateTime','Currency','Dimensions','Weight','Ratio','FreqParameters','power']
+v = 'ad 12,2x12,2x12,2 in'
+match = re.search(r'(^([\d.]+)\s?(lbs|oz|g|kg)$)|\s+([\d.]+)\s?(lbs|oz|g|kg)\s+', str(v))
+if match:
+    match = re.search(r'(^(\d+(?:,\d+)?)x(\d+(?:,\d+)?)(?:x(\d+(?:,\d+)?))?\s?(cms|in|inch|inches|mms)$)|(\d+(?:,\d+)?)x(\d+(?:,\d+)?)(?:x(\d+(?:,\d+)?))?\s?(cms|in|inch|inches|mms)', v)
+    print ('')
+
 # This method eradicates unnecessary tokens and characters for the stream.
 def remove_uncessary_tokens(value):
         value = re.sub(r"[\n\t<>\(\)]+", "", str(value))
@@ -19,47 +24,72 @@ def getDataType(value):
     if ((str(value)).isnumeric()):
         types.append('Numeric')
         return types
+    
     if( (str(value)).lower() in ['true', 'false', 'yes', 'no']):
         types.append('Boolean')
         return types
-
-    types.append('String')
     
     value = str(value).lower();
     
     match = re.search(r'\d{4}-\d{2}-\d{2}', str(value))
     if match:
+        value = re.sub(r'\d{4}-\d{2}-\d{2}', '', str(value))
         types.append('DateTime')
     
     # 12.55$
-    match = re.search(r'^(\$|£|€)?[+-]?[0-9]{1,3}(?:[0-9]*(?:[.,][0-9]{1})?|(?:,[0-9]{3})*(?:\.[0-9]{1,2})?|(?:\.[0-9]{3})*(?:,[0-9]{1,2})?)(\$|£|€)?$', str(value))
+    match = re.search(r'(^(\$|£|€)?[+-]?[0-9]{1,3}(?:[0-9]*(?:[.,][0-9]{1})?|(?:,[0-9]{3})*(?:\.[0-9]{1,2})?|(?:\.[0-9]{3})*(?:,[0-9]{1,2})?)(\$|£|€)?$)|(\s+(\$|£|€)?[+-]?[0-9]{1,3}(?:[0-9]*(?:[.,][0-9]{1})?|(?:,[0-9]{3})*(?:\.[0-9]{1,2})?|(?:\.[0-9]{3})*(?:,[0-9]{1,2})?)(\$|£|€)?\s+)', str(value))
     if match:
+        value = re.sub(r'(^(\$|£|€)?[+-]?[0-9]{1,3}(?:[0-9]*(?:[.,][0-9]{1})?|(?:,[0-9]{3})*(?:\.[0-9]{1,2})?|(?:\.[0-9]{3})*(?:,[0-9]{1,2})?)(\$|£|€)?$)|(\s+(\$|£|€)?[+-]?[0-9]{1,3}(?:[0-9]*(?:[.,][0-9]{1})?|(?:,[0-9]{3})*(?:\.[0-9]{1,2})?|(?:\.[0-9]{3})*(?:,[0-9]{1,2})?)(\$|£|€)?\s+)', '', str(value))
         types.append('Currency')
 
+    # TODO: Add regex for this as well: 64.2cm W x 22.7cm D x 44.0cm H - Weight 8.2kg
     #'12.2x12.2x12.2 in'
-    match = re.search(r'(^[^x\d]+$)|(^.+-)|(\..+$)\s?(cms|in|inch|inches|mms)', str(value))
+    match = re.search(r'(^(\d+(?:,\d+)?)x(\d+(?:,\d+)?)(?:x(\d+(?:,\d+)?))?\s?(cms|in|inch|inches|mms)$)|(\d+(?:,\d+)?)x(\d+(?:,\d+)?)(?:x(\d+(?:,\d+)?))?\s?(cms|in|inch|inches|mms)', str(value))
     if match:
+        value = re.sub(r'(^(\d+(?:,\d+)?)x(\d+(?:,\d+)?)(?:x(\d+(?:,\d+)?))?\s?(cms|in|inch|inches|mms)$)|(\d+(?:,\d+)?)x(\d+(?:,\d+)?)(?:x(\d+(?:,\d+)?))?\s?(cms|in|inch|inches|mms)', '', str(value))
         types.append('Dimensions')
 
     #'15kg'
-    match = re.search(r'([\d.]+)\s?(lbs|oz|g|kg)', str(value))
+    match = re.search(r'(^([\d.]+)\s?(lbs|oz|g|kg)$)|\s+([\d.]+)\s?(lbs|oz|g|kg)\s+', str(value))
     if match:
+        value = re.sub(r'(^([\d.]+)\s?(lbs|oz|g|kg)$)|\s?([\d.]+)\s?(lbs|oz|g|kg)\s+', '', str(value))
         types.append('Weight')
+
+    #'15mm'
+    match = re.search(r'(^([\d.]+)\s?(mm|cm)$)|\s+([\d.]+)\s?(mm|cm)\s+', str(value))
+    if match:
+        value = re.sub(r'(^([\d.]+)\s?(mm|cm)$)|\s?([\d.]+)\s?(mm|cm)\s+', '', str(value))
+        types.append('Length')
+
+    #'15mm'
+    match = re.search(r'(^([\d,]+)\s?(mm|cm)$)|\s+([\d,]+)\s?(mm|cm)\s+', str(value))
+    if match:
+        value = re.sub(r'(^([\d,]+)\s?(mm|cm)$)|\s?([\d,]+)\s?(mm|cm)\s+', '', str(value))
+        types.append('Length')
 
     #'10:10'
     match = re.search(r'\d+\s?:\s?\d+', str(value))
     if match:
+        value = re.sub(r'\d+\s?:\s?\d+', '', str(value))
         types.append('Ratio')
-    match = re.search(r'([\d.]+)\s?(KHz|Hz|hz)', str(value))
+
+    match = re.search(r'([\d.]+)\s?(khz|hz)', str(value))
     if match:
-        types.append('FreqParameters')
+        value = re.sub(r'([\d.]+)\s?(khz|hz)', '', str(value))
+        types.append('Frequency')
+
     match = re.search(r'([\d.]+)\s?(W|w)', str(value))
     if match:
-        types.append('power')
+        value = re.sub(r'([\d.]+)\s?(W|w)', '', str(value))
+        types.append('Power')
 
     ''' Important TODOs
     5. Think about other datatypes as well....
     '''    
+
+    if len(value) > 0:
+        types.append('String')
+    
     return types
 
 
@@ -110,6 +140,7 @@ def entityResolution():
 # This method read all the json files and push the extracted information to a dictionary.
 def extractJSONFiles():
     data = {}
+    keys = {}
     '''
         data = {
             'key' : {                            // Key is concatenated value of 'site-name' and 'source-id'
@@ -134,34 +165,39 @@ def extractJSONFiles():
         }
     
     '''
+    index = 0
     for root, dirs, files in os.walk('../data/data', topdown=False):
         for name in files:
             f = open(os.path.join(root, name), )
             jsondata = json.load(f)
             for key, value in jsondata.items():
                 id = os.path.basename(root) + key
-                if id not in data:                
-                    data[id] = {}
-                    data[id]['Source'] = os.path.basename(root)
-                    data[id]['File'] = name;
-                    data[id]['Frequency'] = 1;
-                    data[id]['Attribute'] = remove_uncessary_tokens(key);
-                    data[id]['Value'] = []
-                    data[id]['Value'].append(remove_uncessary_tokens(value))
-                else:
-                    if not any(str(value) in s for s in data[id]['Value']):
-                        data[id]['Frequency'] = data[id]['Frequency'] + 1
-                        data[id]['Value'].append( remove_uncessary_tokens(value))
+                
+                if id not in keys:
+                    keys[id] = []
+                keys[id].append(index)
+                
+                data[index] = {}
+                data[index]['Source'] = os.path.basename(root)
+                data[index]['File'] = name
+                data[index]['Attribute'] = remove_uncessary_tokens(key)                
+                data[index]['Value'] = remove_uncessary_tokens(value)
+                data[index]['Total Occurrences'] = 1
+                data[index]['TargetAttribute'] = ''
+
                 datatypes = getDataType(value)
                 for dt in datatypes:
-                    data[id][dt] = 1
+                    data[index][dt] = 1
+
+                index = index + 1
             f.close()
-    return data;
+    return data, keys;
 
 # Reading ground-truth file.
-grnd_data={}
-def schemaMatching(data):
-    count=0 
+def schemaMatching(data, keys):
+    index = 0;
+    jsonDataForNN = {}
+    
     dt_grndtruthraw = pd.read_csv('../data/monitor_schema_matching_labelled_data.csv')
     dt_grndtruth = pd.DataFrame(dt_grndtruthraw['source_attribute_id,target_attribute_name'].str.split(',').tolist())
     
@@ -169,39 +205,25 @@ def schemaMatching(data):
     dt_grndtruth[0] = dt_grndtruth[0].apply(lambda x: x.split('//'))
     for row in dt_grndtruth.values.tolist():
         id = row[0][0] + row[0][1]
-        if id in data:
-            for x in data[id]['Value']:
-                grnd_data[count]={}
-                grnd_data[count]['Source'] = data[id]['Source']
-                grnd_data[count]['File'] = data[id]['File'];
-                grnd_data[count]['Frequency'] = data[id]['Frequency'];
-                grnd_data[count]['Attribute'] = data[id]['Attribute'];
-                grnd_data[count]['Value'] = x
-                for dt in allTypes:
-                    if dt in data[id]:
-                        grnd_data[count][dt] = data[id][dt]
-                    else:
-                        grnd_data[count][dt] = 0
-                grnd_data[count]['TargetAttribute'] = row[1]
-                count+=1
-            data[id]['TargetAttribute'] = row[1]
+        if id in keys:
+            for value in keys[id]:
+                data[value]['Total Occurrences'] = len(keys[id])
+                data[value]['TargetAttribute'] = row[1]
+                
+                jsonDataForNN[index] = data[value]
+                index = index + 1
             
-    return data
+    return data, jsonDataForNN
 
 def mergeEntityResolutionInfo(data, pairs, groups):
     #TODO
     return data
 
-jsonData = extractJSONFiles()
+jsonData, keys = extractJSONFiles()
 pairs, groups = entityResolution()
-jsonData = schemaMatching(jsonData)
+jsonData, jsonDataForNN = schemaMatching(jsonData, keys)
 jsonData = mergeEntityResolutionInfo(jsonData, pairs, groups)
 
-df = pd.DataFrame(jsonData).T
+df = pd.DataFrame(jsonDataForNN).T
 df.to_csv('out.csv',index=False)
 print(df)
-
-df_g = pd.DataFrame(grnd_data);
-dfT_g = df_g.T
-dfT_g.to_csv('out_grnd.csv',index=False)
-print(dfT_g)
